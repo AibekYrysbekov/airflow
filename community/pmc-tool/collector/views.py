c6516cec5b564f579199de7153463c704d78d96e
AibekYrysbekov <yrysbekovayba@gmail.com>
2023-05-25 14:50:43 +0600

    add file db.py and update file views.py

--- community/pmc-tool/collector/views.py
+++ community/pmc-tool/collector/views.py
@@ -16,7 +16,7 @@
 # specific language governing permissions and limitations
 # under the License.
 
-"""This file contains logic for interfacing with github and fetching PR and issue data."""
+"""This file contains logic for interfacing with GitHub and fetching PR and issue data."""
 
 #
 # Licensed to the Apache Software Foundation (ASF) under one
@@ -39,8 +39,7 @@
 """This file contains logic for interfacing with github and fetching PR and issue data."""
 
 import requests
-import sqlite3
-
+from db import create_db_connection, create_pull_requests_table, insert_prs_to_db, fetch_pull_requests
 
 # Set up authentication with access token
 with open('token.txt', 'r') as file:
@@ -51,25 +50,20 @@ headers = {'Authorization': f'token {access_token}'}
 # Set up API endpoint
 OWNER = 'apache'
 REPO = 'airflow'
-API_ENDPOINT = f'https://api.github.com/repos/{owner}/{repo}/pulls'
+API_ENDPOINT = f'https://api.github.com/repos/{OWNER}/{REPO}/pulls'
 
 # Connect to the SQLite database
-conn = sqlite3.connect('pull_requests.db')
-c = conn.cursor()
+conn = create_db_connection('pull_requests.db')
 
 # Create a table to store pull requests if it doesn't exist
-c.execute('''
-    CREATE TABLE IF NOT EXISTS pull_requests (
-        author TEXT,
-        count INTEGER
-    )
-''')
+create_pull_requests_table(conn)
 
 # Retrieve list of pull requests
-authors_count = dict()
+authors_count = {}
+
 
 for page_number in range(1, 8):
-    response = requests.get(api_endpoint + f"?page={page_number}", headers=headers, params={'state': 'open'})
+    response = requests.get(API_ENDPOINT + f"?page={page_number}", headers=headers, params={'state': 'open'})
     pull_requests = response.json()
 
     if not pull_requests:
@@ -82,19 +76,17 @@ for page_number in range(1, 8):
         if author not in authors_count:
             authors_count[author] = 0
         authors_count[author] += 1
-
 # Insert pull request data into the database
-for author, count in authors_count.items():
-    c.execute('INSERT INTO pull_requests VALUES (?, ?)', (author, count))
+# insert_prs_to_db(conn, prs=authors_count)
+insert_prs_to_db(conn, prs=[{'author': author, 'count': count} for author, count in authors_count.items()])
 
-c.execute('SELECT author, count FROM pull_requests')
-results = c.fetchall()
-
+# Fetch and print pull request data from the database
+results = fetch_pull_requests(conn)
 for row in results:
     author, count = row
     print(f'Author: {author}, PRs: {count}')
-# Commit the changes and close the database connection
-conn.commit()
+
+# Close the database connection
 conn.close()
 
 print(f'Pull request data saved to the database.')
