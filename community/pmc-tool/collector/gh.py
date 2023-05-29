f79d50f2b674f276cb7fde9c2d9beec80b2f8f63
AibekYrysbekov <yrysbekovayba@gmail.com>
2023-05-29 23:14:31 +0600

    add pydoc to function query_prs

--- community/pmc-tool/collector/gh.py
+++ community/pmc-tool/collector/gh.py
@@ -1,3 +1,24 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+"""This file contains functionality to query data from GitHub"""
+
+
 import requests
 
 # Set up authentication with access token
@@ -18,6 +39,17 @@ def get_api_endpoint():
 
 
 def query_prs(api_endpoint, headers):
+    """
+    Queries the GitHub API for open pull requests.
+
+    Args:
+        api_endpoint (str): The API endpoint to query.
+        headers (dict): Headers containing the authentication token.
+
+    Returns:
+        list: A list of pull requests.
+
+    """
     prs = []
     page_number = 1
 
