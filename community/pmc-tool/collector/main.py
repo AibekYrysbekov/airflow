6bccb761c5096ac2035f035a5ead8da0d918f708
AibekYrysbekov <yrysbekovayba@gmail.com>
2023-06-03 01:24:20 +0600

    update file main.py

--- community/pmc-tool/collector/main.py
+++ community/pmc-tool/collector/main.py
@@ -26,12 +26,12 @@ import logging
 
 
 def assert_equal(db_data, api_data):
-  db_set = set(db_data)
-  api_set = set(api_data.items())
-  if db_set != api_set:
-    raise ValueError('The data from the database does not match the data from the API. '
-        + 'Database has %s extra. ' % db_set - api_set
-        + 'Api has %s extra.' % api_set - db_set)
+    db_set = set(db_data)
+    api_set = set(api_data.items())
+    if db_set != api_set:
+        raise ValueError('The data from the database does not match the data from the API. '
+                         + 'Database has %s extra. ' % (db_set - api_set)
+                         + 'API has %s extra.' % (api_set - db_set))
 
 
 # Connect to the SQLite database
